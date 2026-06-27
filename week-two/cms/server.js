7a5594b1988addde3995de3f5877b5a94b7107e0
Hyrum Whittier <whittierht@gmail.com>
2026-06-27 11:33:27 -0600

    express server

---
+++ week-two/cms/server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var index = require('./server/routes/app');
+var documents = require('./server/routes/documents');
+var messages = require('./server/routes/messages');
+var contacts = require('./server/routes/contacts');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'dist/cms/browser')));
+
+app.use('/', index);
+app.use('/documents', documents);
+app.use('/messages', messages);
+app.use('/contacts', contacts);
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
+});
+
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port, function() {
+  console.log('API running on localhost: ' + port);
+});
